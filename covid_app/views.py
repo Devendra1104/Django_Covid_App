7b4b04535697c3448e02fa041105c655e523c44f
Devendra1104 <devendrapanchal58@gmail.com>
2020-04-15 22:46:07 +0530

    first commit

---
+++ covid_app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import requests
+import json
+
+def home(request):
+
+    if request.method == 'POST':
+        country = request.POST['country']
+        url = 'https://corona.lmao.ninja/countries/'+country
+        response = requests.get(url)
+        jres = json.loads(response.text)
+        print(jres)
+        # country = 
+        # case = 
+        data = {'Country':jres['country'],
+                'Cases':str(jres['cases']),
+                'Deaths':str(jres['deaths']),
+                'Active':str(jres['active']),
+                'Critical':str(jres['critical']),
+                'TodaysDeath':str(jres['todayDeaths']),
+                'TodayCases':str(jres['todayCases']),
+        }
+
+        print(data)
+    else:
+        data = {}
+        
+    return render(request,'home.html',data)
