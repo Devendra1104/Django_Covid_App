b6c68727f93f9b2d1081358d85ce0b673c750f82
Devendra1104 <devendrapanchal58@gmail.com>
2020-04-18 19:15:59 +0530

    Added condition for invalid input

--- covid_app/views.py
+++ covid_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import requests
 import json
 
@@ -7,21 +7,28 @@ def home(request):
     if request.method == 'POST':
         country = request.POST['country']
         url = 'https://corona.lmao.ninja/v2/countries/'+country
-        response = requests.get(url)
-        jres = json.loads(response.text)
-        print(jres)
-        # country = 
-        # case = 
-        data = {'Country':jres['country'],
-                'Cases':str(jres['cases']),
-                'Deaths':str(jres['deaths']),
-                'Active':str(jres['active']),
-                'Critical':str(jres['critical']),
-                'TodaysDeath':str(jres['todayDeaths']),
-                'TodayCases':str(jres['todayCases']),
-        }
 
-        print(data)
+
+        response = requests.get(url)
+
+        if response:
+            jres = json.loads(response.text)
+            print(jres)
+            # country = 
+            # case = 
+            data = {'Country':jres['country'],
+                    'Cases':str(jres['cases']),
+                    'Deaths':str(jres['deaths']),
+                    'Active':str(jres['active']),
+                    'Critical':str(jres['critical']),
+                    'TodaysDeath':str(jres['todayDeaths']),
+                    'TodayCases':str(jres['todayCases']),
+            }
+            print(data)
+        
+        else:
+            return render(request,'Invalid.html')
+
     else:
         data = {}
         
--- covid_project/urls.py
+++ covid_project/urls.py
@@ -20,4 +20,5 @@ from covid_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('invalid/',views.home,name='home'),
 ]
